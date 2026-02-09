0bfe55ff4352b0dc5728fc681d94825d405989db
rahmatilla2002rrr-cmd <rahmatilla2002rrr@gmail.com>
2026-02-09 10:37:58 +0500

    main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import Command
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def start_handler(message: Message):
+    await message.answer("✅ Bot ishlayapti. Kanal keyingi bosqichda ulanadi.")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
